a0e7e0ec35aad850d51a152a115cf34e27a1821b
ogcraft <ogcraft@gmail.com>
2013-12-25 17:14:08 +0200

    Going from strings to lists

--- sudoku/sudoku.lisp
+++ sudoku/sudoku.lisp
@@ -9,11 +9,11 @@
 ;rows     = 'ABCDEFGHI'
 ;cols     = digits
 
-(defconstant +digits+ "12345678")
-(defconstant +rows+ "ABCDEFGHI")
-(defconstant +cols+ digits)
+(defconstant +digits+ '("1" "2" "3" "4" "5" "6" "7" "8"))
+(defconstant +rows+ '("A" "B" "C" "D" "E" "F" "G" "H" "I"))
+(defconstant +cols+ +digits+)
 
-
+(defun cross ( a b ))
 
 
 
